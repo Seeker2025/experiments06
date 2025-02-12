99968713efca138e11c9d3ecc168d781ac6a4d24
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-02-12 20:59:30 +0200

    zzz

--- src/index.js
+++ src/index.js
@@ -1,69 +1,14 @@
-console.log('Hello, world!');
-console.log('Hello, world!');
+import * as basicLightbox from 'basiclightbox';
+import "basiclightbox/dist/basicLightbox.min.css";
+
+import { common } from './js/common';
+import { instruments } from './js/inst';
+
+// console.log('Hello, world!');
+console.log(common);
+console.log(instruments);
 
 
-const instruments = [
-     {
-  id: 1,
-  img: "https://static.dnipro-m.ua/cache/products/4878/catalog_origin_325618.jpg",
-  name: "Шуруповёрт",
-  price: 1500,
-  description: "Мережевий дриль-шуруповерт ТD-30-Надійний помічник для робіт по дому"
-     },
-     {
-  id: 2,
-  img: "https://static.dnipro-m.ua/cache/products/10774/catalog_origin_486919.jpg",
-  name: "Перфоратор",
-  price: 3948,
-  description: "Перфоратор бочковий Dnipro-M BH-20 Використовується для свердління"
-    },
-
-      {
- id: 3,
- img: "https://static.dnipro-m.ua/cache/products/1248/catalog_origin_320661.jpg",
- name: "Шліфмашина",
- price: 1299,
- description: "Кутова шліфмашина Dnipro-M GS-98 - модель яка поєднує в собі"
-   },
-    {
- id: 4,
- img: "https://static.dnipro-m.ua/cache/products/4406/catalog_origin_322116.jpg",
- name: "Пила",
- price: 11049,
- description: "Мобільна акумуляторна ланцюгова пила DCS-200BC DUAL"
-   }, 
-   {
- id: 5,
- img: "https://static.dnipro-m.ua/cache/products/2300/catalog_origin_326088.jpg",
- name: "Рівень",
- price: 897,
- description: "Рівень серії ProVision виробництва DNIPRO-M"
-    },
-  
-    {
- id: 6,
- img: "https://static.dnipro-m.ua/cache/products/6566/catalog_origin_476205.jpg",
- name: "Тример",
- price: 3699,
- description: "Тример електричний Dnipro-M 110 призначений для покосу трави"
-    },
-     
-    {
- id: 7,
- img: "https://static.dnipro-m.ua/cache/products/6483/catalog_origin_325859.jpg",
- name: "Мотокоса",
- price: 11049,
- description: "Мотокоса Dnipro-M 43 призначений для покосу трави, чагарників, бур'янів"
-    },
-
-    {
-        id: 8,
- img: "https://static.dnipro-m.ua/cache/products/2741/catalog_origin_318327.jpg",
- name: "Генератор",
- price: 10890,
- description: "Бензиновый генератор Dnipro-M GX-25 номінальною потужністю 2,5 кВт"
-    },  
-];
 
 
 // console.log(instruments);
@@ -72,21 +17,80 @@ const search = document.querySelector('.js-search');
 const list = document.querySelector('.js-list');
 console.log(search);
 console.log(list);
+const favoriteArr = [];
+const basketArr = [];
+
 
 function createMarkup(arr) {
-   const markup = arr.map(({ id, img, name}) => `<li class="border" data-id="${id}">
+   const markup = arr.map(({ id, img, name}) => `<li class="card js-card" data-id="${id}">
         <img src="${img}" alt="${name}" width="200" >
         <h2>${name}</h2>
-        <p>More information</p>
-        <div>
+        <p class="class_info js-info"><a href="#" class="js-info">More information</a></p>
+            <div>
 <button>Add to favorite</button>
 <button>Add to basket</button>
-        </div>
+            </div>
         </li>
   
  <li>`).join('');
    
    list.innerHTML = markup;
 }  
-     
-createMarkup(instruments)
+
+list.addEventListener('click', onClick)
+
+function onClick(event) {
+   event.preventDefault();
+   // console.log(event.target)
+   if (event.target.classList.contains('js-info')) {
+      // const { id } = event.target.closest('.js-card').dataset;
+      // console.log('This is id:', id);
+      console.log(event.target)
+      const product = findProduct(event.target);
+      // console.log(product);
+      const{ img, name, price, description } = product;
+      const instance = basicLightbox.create(`
+
+         <div class="box">
+               <img src="${img}" alt="${name}" width="400">
+               <h2 class="boo">${name}</h2>
+               <h3>${price} грн</h3>
+               <p>${description}</p>
+               <div>
+<button class="js-favorite">Add to favorite</button>
+<button class="js-basket">Add to basket</button>
+<div >Close</div>
+               </div>
+         </div>
+
+         `);
+      instance.show();
+   }
+//    const dddds= document.querySelector('.js-favorite');
+// const ddddddsds = document.querySelector('.js-basket');
+// console.log(dddds);
+   // console.log(ddddddsds);
+   if (event.target.classList.contains('boo')) {
+     console.log('dddds');
+
+   }
+   if (event.target.classList.contains('js-basket')) {
+      console.log(event.target);
+      const product = findProduct(event.target);
+      basketArr.push(product);
+      localStorage.setItem(KEY_BASKET, JSON.stringify(basketArr));
+   }
+   console.log(event.currentTarget);
+} 
+createMarkup(instruments);
+
+
+ 
+function findProduct(elem) {
+   const { id } = elem.closest('.js-card').dataset;
+   const productId = Number(id);
+   return instruments.find(({ id }) => id === productId);
+}
+
+console.log(favoriteArr);
+
---
+++ src/js/common.js
@@ -0,0 +1,5 @@
+const common = {
+    KEY_FAVORITE: 'favorite',
+    KEY_BASKET: 'basket'
+}
+export {common};
