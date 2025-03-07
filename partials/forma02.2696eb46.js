fdc243ed280787cedd1e0d778a42e8e3c2dfd49f
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-03-07 18:39:31 +0000

    Deploying to gh-pages from @ Seeker2025/experiments06@018009c545a299b9546b89c8fc13887760231336 🚀

---
+++ partials/forma02.2696eb46.js
@@ -0,0 +1,2 @@
+console.log("w");const e=document.querySelector(".feedback-form");e.addEventListener("input",(e=>{localStorage.setItem("goit-example-message",e.target.value)})),e.addEventListener("submit",(t=>{t.preventDefault(),localStorage.removeItem("goit-example-message"),e.reset()}));
+//# sourceMappingURL=forma02.2696eb46.js.map
---
+++ partials/forma02.ebb26e94.js
@@ -0,0 +1,2 @@
+!function(){console.log("w");var e=document.querySelector(".feedback-form"),t="goit-example-message";e.addEventListener("input",(function(e){localStorage.setItem(t,e.target.value)})),e.addEventListener("submit",(function(o){o.preventDefault(),localStorage.removeItem(t),e.reset()}))}();
+//# sourceMappingURL=forma02.ebb26e94.js.map
